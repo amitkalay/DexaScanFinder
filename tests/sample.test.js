17e601535b98b82e241daad32273fd092a549e02
Amit Kalay <amitkalay8@gmail.com>
2026-04-24 18:24:48 -0700

    Scope CI to main pull requests

--- tests/sample.test.js
+++ tests/sample.test.js
@@ -1,6 +1,6 @@
 const assert = require("node:assert/strict");
 const test = require("node:test");
 
-test("sample failing assertion", () => {
-  assert.equal(2, 4);
+test("sample assertion", () => {
+  assert.equal(2, 2);
 });
